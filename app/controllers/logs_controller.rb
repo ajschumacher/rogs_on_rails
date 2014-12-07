5eca0938031451e7832a26d86900ecab816ab956
Aaron Schumacher <ajschumacher@gmail.com>
2014-12-07 17:29:11 -0500

    get post body into database

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -3,6 +3,8 @@ class LogsController < ApplicationController
   protect_from_forgery except: :index
 
   def create
+    new_log = Log.new(entry: request.raw_post)
+    new_log.save
     render 'index'
   end
 
